f586a3f3c0ba589cabdb6b8bd2d2f262e695d57f
SofiaCardenas2302 <sofyalejandracardenas@gmail.com>
2026-07-01 21:39:41 -0500

    Create Tres_paramétros.py

---
+++ Tres_paramétros.py
@@ -0,0 +1,8 @@
+def address(street, city, postal_code):
+    print("Tu dirección es:", street,"St.,", city, postal_code)
+
+s = input("Calle: ")
+p_c = input("Código Postal: ")
+c = input("Ciudad: ")
+address(s, c, p_c)
+
