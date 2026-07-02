8e0178889cc87a4154d287b8d66bc995929d54b7
SofiaCardenas2302 <sofyalejandracardenas@gmail.com>
2026-07-01 21:41:13 -0500

    Create Argumentos_a_funcion.py

---
+++ Argumentos_a_funcion.py
@@ -0,0 +1,22 @@
+Ejemplo 1
+def subtra(a, b):
+    print(a - b)
+
+subtra(5, 2)    # salida: 3
+subtra(2, 5)    # salida: -3
+
+
+Ejemplo 2
+def subtra(a, b):
+    print(a - b)
+
+subtra(a=5, b=2)    # salida: 3
+subtra(b=2, a=5)    # salida: 3
+
+Ex. 3
+def subtra(a, b):
+    print(a - b)
+
+subtra(5, b=2)    # salida: 3
+subtra(5, 2)    # salida: 3
+
