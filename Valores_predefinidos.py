0ac87d0b20534e342d9b9e430c3794d433d7bc69
SofiaCardenas2302 <sofyalejandracardenas@gmail.com>
2026-07-01 21:43:43 -0500

    Create Valores_predefinidos.py

---
+++ Valores_predefinidos.py
@@ -0,0 +1,6 @@
+def name(first_name, last_name="Pérez"):
+    print(first_name, last_name)
+
+name("Andy")    # salida: Andy Pérez
+name("Bety", "Rodríguez")    # salida: Bety Rodríguez (el argumento de palabra clave es reemplazado por "Rodríguez")
+
